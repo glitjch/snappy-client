95bf6b38a38e72331bc68e0672bf5e8156d92a8a
TJ Phan <t.jamesphan@gmail.com>
2022-05-16 17:45:10 -0700

    Commit first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.REACT_APP_OPENAI_API_KEY
+});
+const openai = new OpenAIApi(configuration);
+const response = openai.listEngines();
+
 
 function App() {
+  const [responseData, setResponseData] = useState()
+
+  const handleClick = () =>{
+    setResponseData(response)
+  }
+
+  useEffect(() => {
+    console.log(responseData)
+  }, [responseData])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() =>handleClick()}>click me</button>
+      {responseData && "DID IT" + responseData}
     </div>
   );
 }
