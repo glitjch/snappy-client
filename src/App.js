c2bb396f8f7c9ec5ba6a864440594c05e30cb3e8
TJ Phan <t.jamesphan@gmail.com>
2022-05-17 13:38:15 -0700

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { useState } from "react";
-// import Instructions from "./Instructions";
-
-
+import Instructions from "./Instructions";
 
 // COMPONENT
 function App() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+  const [ userDataInput, setUserDataInput ] = useState("")
+  const [ result, setResult ] = useState("Empty result")
 
-  async function onSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
     const response = await fetch("/cors", {
       mode:'cors',
@@ -16,68 +14,31 @@ function App() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ animal: animalInput }),
+      body: JSON.stringify({ user: userDataInput }),
     });
     const data = await response.json();
     setResult(data.result);
-    setAnimalInput("");
+    setUserDataInput("");
   }
 
   return (
     <div>
       <main >
-        <h3>Name my pet</h3>
-        <form onSubmit={onSubmit}>
+        <Instructions />
+        <form onSubmit={handleSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="user"
+            placeholder="Follow the instructions"
+            value={userDataInput}
+            onChange={(e) => setUserDataInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+           <button type="submit">Generate Pitch</button>
         </form>
         <div >{result}</div>
       </main>
     </div>
   );
-  // const [ userDataInput, setUserDataInput ] = useState("")
-  // const [ result, setResult ] = useState("Empty result")
-
-  // async function handleSubmit(event) {
-  //   event.preventDefault();
-  //   const response = await fetch('/cors', {
-  //     mode:"cors", 
-  //     method:"POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({ animal: userDataInput })
-  //   });
-  //   const data = await response.json();
-  //   console.log(data)
-  //   setResult(data.result);
-  //   setUserDataInput("");
-  // }
-
-  // // VIEW
-  // return (
-  //   <div>
-  //     <Instructions />
-  //     <form onSubmit={handleSubmit}>
-  //       <input
-  //         type="text"
-  //         name="user"
-  //         placeholder="Enter your info"
-  //         value={userDataInput}
-  //         onChange={(e)=> setUserDataInput(e.target.value)}
-  //       />
-  //       <button type="submit">Generate Pitch</button>
-  //     </form>
-  //     <div>{userDataInput}</div>
-  //     <div>{result}</div>
-  //   </div>
-  // );
-}
+};
 
 export default App;
