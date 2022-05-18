1baf47139d5b500653d9470ddfdee2c12473387f
TJ Phan <t.jamesphan@gmail.com>
2022-05-18 01:58:35 -0700

    Add error message when nothing new typed

--- src/App.js
+++ src/App.js
@@ -8,10 +8,15 @@ function App() {
   const [ userDataInput, setUserDataInput ] = useState(
     `name: \ncareer: \nspecialty: \njob-description: \ngoal: `);
   const [ result, setResult ] = useState("No pitch yet. Follow the Instructions!");
+  const [error, setError] = useState("")
 
 
   async function handleSubmit(event) {
     event.preventDefault();
+    if (event.target[0].value === `name: \ncareer: \nspecialty: \njob-description: \ngoal: `) {
+      setError("Please enter your info after each prompt (name: , career: , etc.)");
+      return;
+    }
     const response = await fetch("/pitch", {
       mode:'cors',
       method: "POST",
@@ -34,10 +39,14 @@ function App() {
             name="user"
             placeholder="Follow the instructions"
             value={userDataInput}
-            onChange={(e) => setUserDataInput(e.target.value)}
+            onChange={(e) => {
+              setError("");
+              setUserDataInput(e.target.value)
+              }
+            }
           />
            <button type="submit">Generate Pitch</button>
-          <span >{result}</span>
+          <span className={error ? styles.error : styles.message}>{!error ? result : error}</span>
         </form>
       </main>
   );
